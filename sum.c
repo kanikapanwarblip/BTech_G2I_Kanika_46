d12a95f08cd277427538df14244fd8a7d05d92d7
kanikapanwarblip <kanikathakurpanwar@gmail.com>
2026-02-12 09:44:50 +0530

    sum.c

---
+++ sum.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+    int s1, s2,s3,s4,s5, sum;
+    float p;
+    printf("enter marks of five subject =");
+    scanf("5d5d5d5d5d", &s1,&s2,&s3,&s4,&s5,),
+    sum = s1+s2+s3+s4+s5;
+    p = sum/50
+    printf("sum=%d\n percentage=2f" ,sum, p);
+
+    printf("the sum of a and b = %d",c);
+
+    return 0;
+
+
+}
