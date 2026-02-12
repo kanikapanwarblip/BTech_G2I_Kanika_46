6fcac6f53a4f08caa2afb51b4e9095ecb9bc2fb6
kanikapanwarblip <kanikathakurpanwar@gmail.com>
2026-02-12 10:15:55 +0530

    c.sum

---
+++ c.sum.c
@@ -0,0 +1,12 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+int x,y,z;
+printf("I created a new GitHub account today\n");
+    printf("Enter the value of x and y");
+scanf("%d%d", &x,&y);
+z=x+y;
+printf("The sum of x and y is %d",z);
+    return 0;
+}
